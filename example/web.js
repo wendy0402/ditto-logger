373a8eba0147edb3273bcc244534d6c1e3dd43e3
wendy0402 <wendykurniawan92@gmail.com>
2015-04-25 09:49:16 +0700

    create basic logger

---
+++ example/web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var logger = require('../index.js');
+var url = require('url');
+var bodyParser = require('body-parser');
+
+
+app.use(logger);
+
+app.get('/', function(req, res){
+  res.end('foo bar');
+});
+
+app.post('/post', function(req, res){
+  res.end('foo post');
+});
+
+app.listen(4040, function(){
+  console.log("Listening on 0.0.0.0:4040, CTRL+C to stop");
+});
---
+++ index.js
@@ -0,0 +1,70 @@
+var strftime = require('strftime');
+var onFinished = require('on-finished');
+var url = require('url');
+var httpStatus = require('http-status');
+
+module.exports =  exports = function(req, res, next){
+
+  onFinished(req, function(err, req){
+    if(err){
+      process.stdout.write(err);
+      return
+    }
+    process.stdout.write(createReqFormat(req));
+  });
+
+  res.on('finish', function(){
+    process.stdout.write(createResFormat(res));
+  });
+  next();
+};
+
+
+function createReqFormat(req) {
+  return compileRequest("\nStarted %pathName for %ipAddr at %time", req);
+}
+
+function createResFormat(res) {
+  return compileResponse("Completed %statusCode %statusMessage", res);
+}
+
+function compileRequest(format, req){
+  return format.replace(/%\S+/g, function(str){
+    str = str.replace('%', '');
+    return exports.codeReqList[str](req);
+  });
+}
+
+function compileResponse(format, res){
+  return format.replace(/%\S+/g, function(str){
+    str = str.replace('%', '');
+    return exports.codeResList[str](res);
+  });
+}
+
+
+exports.codeReqList = {}
+exports.codeResList = {}
+
+exports.codeReqList.pathName = function (req){
+  return url.parse(req.url).pathname;
+}
+
+exports.codeReqList.time = function (req){
+  var date = new Date();
+  return date.toString();
+}
+
+
+exports.codeReqList.ipAddr = function (req){
+  return req.ip || req.connection.remoteAddress || req.socket.remoteAddress ||
+  (req.connection.socket && req.connection.socket.remoteAddress) || undefined;
+}
+
+exports.codeResList.statusCode = function(res){
+  return res.statusCode;
+}
+
+exports.codeResList.statusMessage = function(res){
+  return httpStatus[res.statusCode];
+}
